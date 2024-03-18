aff046c67f9be5eb9652fa0d373c387431f3020c
* <m.farrellm.farrell.13@gmail.com>
2024-03-18 22:11:01 +0700

    return instead of printing

--- lib/pdf2tex.ex
+++ lib/pdf2tex.ex
@@ -2,21 +2,18 @@ defmodule PDF2tex do
   def retain_layout(input_pdf) do
     command = "pdftotext"
     args = ["-layout", input_pdf, "-"]
-
     execute_pdftotext(command, args)
   end
 
   def raw_text(input_pdf) do
     command = "pdftotext"
     args = ["-raw", input_pdf, "-"]
-
     execute_pdftotext(command, args)
   end
 
   defp execute_pdftotext(command, args) do
     case System.cmd(command, args) do
       {output, 0} ->
-        IO.puts(output)
         {:ok, output}
 
       {output, _} ->
@@ -24,3 +21,4 @@ defmodule PDF2tex do
     end
   end
 end
+
