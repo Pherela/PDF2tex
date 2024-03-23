1b3b31c86892edfca011c3cacf73f5ca624b8c91
* <m.farrellm.farrell.13@gmail.com>
2024-03-24 03:25:18 +0700

    retain_layout and raw_text functions take an optional start_page and end_page.

--- lib/pdf2tex.ex
+++ lib/pdf2tex.ex
@@ -1,23 +1,52 @@
 defmodule PDF2tex do
-  def retain_layout(input_pdf) do
-    command = "pdftotext"
+  @moduledoc """
+  A module for converting PDF files to text using the `pdftotext` command.
+  """
+
+  @doc """
+  Converts a PDF file to text while retaining the layout.
+
+  ## Parameters
+
+  - `input_pdf`: The path to the input PDF file.
+  - `start_page`: (Optional) The first page to convert. If not provided, conversion starts from the first page.
+  - `end_page`: (Optional) The last page to convert. If not provided, conversion goes up to the last page.
+  """
+  def retain_layout(input_pdf, start_page \\ nil, end_page \\ nil) do
     args = ["-layout", input_pdf, "-"]
-    execute_pdftotext(command, args)
+    args = cond do
+      start_page && end_page -> ["-f", Integer.to_string(start_page), "-l", Integer.to_string(end_page) | args]
+      start_page -> ["-f", Integer.to_string(start_page) | args]
+      end_page -> ["-l", Integer.to_string(end_page) | args]
+      true -> args
+    end
+    execute_pdftotext(args)
   end
 
-  def raw_text(input_pdf) do
-    command = "pdftotext"
+  @doc """
+  Converts a PDF file to raw text.
+
+  ## Parameters
+
+  - `input_pdf`: The path to the input PDF file.
+  - `start_page`: (Optional) The first page to convert. If not provided, conversion starts from the first page.
+  - `end_page`: (Optional) The last page to convert. If not provided, conversion goes up to the last page.
+  """
+  def raw_text(input_pdf, start_page \\ nil, end_page \\ nil) do
     args = ["-raw", input_pdf, "-"]
-    execute_pdftotext(command, args)
+    args = cond do
+      start_page && end_page -> ["-f", Integer.to_string(start_page), "-l", Integer.to_string(end_page) | args]
+      start_page -> ["-f", Integer.to_string(start_page) | args]
+      end_page -> ["-l", Integer.to_string(end_page) | args]
+      true -> args
+    end
+    execute_pdftotext(args)
   end
 
-  defp execute_pdftotext(command, args) do
-    case System.cmd(command, args) do
-      {output, 0} ->
-        output
-
-      {output, _} ->
-        output
+  defp execute_pdftotext(args) do
+    case System.cmd("pdftotext", args) do
+      {output, 0} -> output
+      {output, _} -> output
     end
   end
 end
